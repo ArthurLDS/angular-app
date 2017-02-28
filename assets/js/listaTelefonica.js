f035706147476b4edf77ad777ec4892a1479329b
ArthurLDS <arthurlds73@gmail.com>
2017-02-27 22:39:15 -0300

    feat(index, listaTelefonicaJS): implementar ordenação de contatos.

--- assets/js/listaTelefonica.js
+++ assets/js/listaTelefonica.js
@@ -11,22 +11,28 @@ function ListaTelefonicaController($scope){
   ];
 
   $scope.contatos = [
-    {nome : "Arthur", telefone : 4434533, operadora: $scope.operadoras[0]},
-    {nome : "Mara", telefone : 33453433, operadora: $scope.operadoras[2]},
-    {nome : "Rafael", telefone : 3354453, operadora: $scope.operadoras[1]}
+    {nome : "Arthur", telefone : 4434533, operadora: $scope.operadoras[0], data: new Date(2016, 10, 27)},
+    {nome : "Mara", telefone : 33453433, operadora: $scope.operadoras[2] , data: new Date()},
+    {nome : "Rafael", telefone : 3354453, operadora: $scope.operadoras[1], data: new Date()},
+    {nome : "Larri", telefone : 5654453, operadora: $scope.operadoras[1], data: new Date()}
   ];
 
   $scope.adicionar = function(contato){
     $scope.contatos.push(contato);
     delete $scope.contato;
     $scope.contatoForm.$setPristine(true);
-  }
+  };
 
   $scope.remover = function(contatos){
     $scope.contatos = contatos.filter(c => !c.selecionado);
-  }
+  };
 
   $scope.isContatoSelecionado = function(contatos){
     return contatos.some(c => c.selecionado);
-  }
+  };
+
+  $scope.ordernarPor = function(campo){
+    $scope.criterioDeOrdenacao = campo;
+    $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+  };
 }
