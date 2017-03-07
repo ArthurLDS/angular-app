04adc10082d74fe211477489985852f46ca23d91
ArthurLDS <arthurlds73@gmail.com>
2017-03-07 17:17:05 -0300

    feat(uiAccodionDirective): implementar diretiva para abertura e fechamento de accordion.

---
+++ app/js/directives/uiAccordionDirective.js
@@ -0,0 +1,33 @@
+angular.module("listaTelefonica").directive("uiAccordions", function(){
+  return {
+    controller: function($scope, $element, $attrs){
+      let accordions = [];
+
+      this.registerAccordion = function (scope) {
+        accordions.push(scope);
+      }
+      this.closeAll = function() {
+        accordions.forEach(a => a.isOpened = false);
+      }
+    }
+  }
+});
+
+angular.module("listaTelefonica").directive("uiAccordion", function(){
+  return{
+      restrict: "E",
+      require: "^uiAccordions",
+      templateUrl: "view/accordion.html",
+      transclude : true,
+      scope: {
+        title : "@"
+      },
+      link: function (scope, element, attrs, ctrl){
+        ctrl.registerAccordion(scope);
+        scope.open = function() {
+          ctrl.closeAll();
+          scope.isOpened = !scope.isOpened;
+        }
+      }
+  };
+});
