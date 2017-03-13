30dd9d399448ba243c618780c9d520de627a5700
ArthurLDS <arthurlds73@gmail.com>
2017-03-13 17:28:00 -0300

    feat(routeConfig): configurar rota para detalhes de contato.

--- app/static/js/config/routeConfig.js
+++ app/static/js/config/routeConfig.js
@@ -1,11 +1,23 @@
 angular.module("listaTelefonica").config(function ($locationProvider, $routeProvider) {
   
-  $routeProvider.when("/contacts", {
-      templateUrl : 'view/contatos.html'
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
+  $routeProvider
+    .when("/contacts", {
+      templateUrl : 'view/contatos.html',
+      controller: 'listaTelefonicaController',
+      resolve: {
+          contatos : function(contatosAPI){
+            return contatosAPI.getContatos();
+          }
+      }
+    })
+    .when("/detalhesContato/:id", {
+      templateUrl : 'view/detalhesContato.html'
+      
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+ 
   $locationProvider.html5Mode(true);
   
 });
