7d78cdebc47638f23ff96f26eeced9953db74570
ArthurLDS <arthurlds73@gmail.com>
2017-03-13 17:27:25 -0300

    get(contatosAPIService): implementar método getContato.

--- app/static/js/services/contatosAPIService.js
+++ app/static/js/services/contatosAPIService.js
@@ -7,9 +7,14 @@ angular.module("listaTelefonica").factory("contatosAPI", function ($http, config
   var _saveContato = function(contato) {
     return $http.put(config.baseUrl + '/addContato', contato);
   };
+  
+  var _getContato = function(id) {
+     return $http.get(config.baseUrl + '/contato/?id=' + id); 
+  };
 
   return {
     getContatos : _getContatos,
+    getContato  : _getContato,
     saveContato : _saveContato
   };
 });
